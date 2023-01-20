593202ecf5ab05931f6baae1242dd613e43e580c
Timur-hs <timur-Khasanov23@yandex.ru>
2023-01-20 18:27:52 +0300

    profile 5.0

--- resources/js/views/compiler/Compiler.vue
+++ resources/js/views/compiler/Compiler.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="col-md-9">
     <p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="NWGZzXx" data-preview="true" data-editable="true" data-user="aaroniker" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
   <span>See the Pen <a href="https://codepen.io/aaroniker/pen/NWGZzXx">
   Paper plane button</a> by Aaron Iker (<a href="https://codepen.io/aaroniker">@aaroniker</a>)
@@ -10,6 +11,7 @@
 <!--    </div>-->
 <!--    </body>-->
 <!--    <iframe src="https://trinket.io/embed/html/7bfc6f1c3d?runMode=autorun" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>-->
+    </div>
 </template>
 <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
 <script>
