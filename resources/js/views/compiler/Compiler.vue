d570d0643ff80022707e961a89a56ac8a048fa0f
Idris <astralgg@mail.ru>
2023-01-20 18:27:47 +0300

    t
    t

--- resources/js/views/compiler/Compiler.vue
+++ resources/js/views/compiler/Compiler.vue
@@ -1,17 +1,17 @@
 <template>
-    <p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="NWGZzXx" data-preview="true" data-editable="true" data-user="aaroniker" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-  <span>See the Pen <a href="https://codepen.io/aaroniker/pen/NWGZzXx">
-  Paper plane button</a> by Aaron Iker (<a href="https://codepen.io/aaroniker">@aaroniker</a>)
-  on <a href="https://codepen.io">CodePen</a>.</span>
-    </p>
+<!--    <p class="codepen" data-height="300" data-default-tab="html,result" data-slug-hash="NWGZzXx" data-preview="true" data-editable="true" data-user="aaroniker" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">-->
+<!--  <span>See the Pen <a href="https://codepen.io/aaroniker/pen/NWGZzXx">-->
+<!--  Paper plane button</a> by Aaron Iker (<a href="https://codepen.io/aaroniker">@aaroniker</a>)-->
+<!--  on <a href="https://codepen.io">CodePen</a>.</span>-->
+<!--    </p>-->
 <!--    <body>-->
 <!--    <div data-pym-src='https://www.jdoodle.com/plugin' data-language="python3"-->
 <!--         data-version-index="3">-->
 <!--    </div>-->
 <!--    </body>-->
-<!--    <iframe src="https://trinket.io/embed/html/7bfc6f1c3d?runMode=autorun" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>-->
+    <iframe src="https://trinket.io/embed/html/7bfc6f1c3d?runMode=autorun" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
 </template>
-<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+<!--<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>-->
 <script>
 export default {
     name: "Compiler"
