63ef017b78ea32cd5d22f3ca85efc3a88dc6ea47
arturhasahanov <arturbk2@yandex.ru>
2023-01-24 09:24:30 +0300

    commit accordion for timur

--- resources/js/views/courses/Courses.vue
+++ resources/js/views/courses/Courses.vue
@@ -3,7 +3,7 @@
     <div class="col-md-9">
 
         <!-- Course item START -->
-        <div v-for="course in courses" class="card border">
+        <div v-for="course in courses" class="card border mt-4">
             <div class="card-header border-bottom">
                 <!-- Card START -->
                 <div class="card">
@@ -40,30 +40,30 @@
                 <h5>Course Curriculum</h5>
 
                 <!-- Accordion START -->
-                <div class="accordion accordion-icon accordion-bg-light" id="accordionExample2">
+                <div v-for="lesson in course.lessons" class="accordion accordion-icon accordion-bg-light" id="accordionExample2">
                     <!-- Item -->
                     <div  class="accordion-item mb-3">
                         <h6 class="accordion-header font-base" id="heading-1">
                             <a class="accordion-button fw-bold rounded collapsed d-block pe-4" href="#collapse-1" data-bs-toggle="collapse" data-bs-target="#collapse-1" aria-expanded="true" aria-controls="collapse-1">
-                                <span class="mb-0">Introduction of Digital Marketing</span>
-                                <span class="small d-block mt-1">(3 Lectures)</span>
+                                <span class="mb-0">Урок</span>
+                                <span class="small d-block mt-1">{{lesson.lesson_number}}</span>
                             </a>
                         </h6>
-                        <div id="collapse-1" class="accordion-collapse collapse show" aria-labelledby="heading-1" data-bs-parent="#accordionExample2">
+                        <div class="accordion-collapse collapse show" aria-labelledby="heading-1" data-bs-parent="#accordionExample2">
                             <div class="accordion-body mt-3">
                                 <div class="vstack gap-3">
 
-                                    <!-- Progress bar -->
-                                    <div class="overflow-hidden">
-                                        <div class="d-flex justify-content-between">
-                                            <p class="mb-1 h6">1/2 Completed</p>
-                                            <h6 class="mb-1 text-end">80%</h6>
-                                        </div>
-                                        <div class="progress progress-sm bg-primary bg-opacity-10">
-                                            <div class="progress-bar bg-primary aos" role="progressbar" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
-                                            </div>
-                                        </div>
-                                    </div>
+<!--                                    &lt;!&ndash; Progress bar &ndash;&gt;-->
+<!--                                    <div class="overflow-hidden">-->
+<!--                                        <div class="d-flex justify-content-between">-->
+<!--                                            <p class="mb-1 h6">1/2 Completed</p>-->
+<!--                                            <h6 class="mb-1 text-end">80%</h6>-->
+<!--                                        </div>-->
+<!--                                        <div class="progress progress-sm bg-primary bg-opacity-10">-->
+<!--                                            <div class="progress-bar bg-primary aos" role="progressbar" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
 
                                     <!-- Course lecture -->
                                     <div>
@@ -195,71 +195,71 @@
                     </div>
 
                     <!-- Item -->
-                    <div class="accordion-item mb-3">
-                        <h6 class="accordion-header font-base" id="heading-2">
-                            <a class="accordion-button fw-bold collapsed rounded d-block pe-4" href="#collapse-2" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false" aria-controls="collapse-2">
-                                <span class="mb-0">Customer Life cycle</span>
-                                <span class="small d-block mt-1">(3 Lectures)</span>
-                            </a>
-                        </h6>
-                        <div id="collapse-2" class="accordion-collapse collapse" aria-labelledby="heading-2" data-bs-parent="#accordionExample2">
-                            <!-- Accordion body START -->
-                            <div class="accordion-body mt-3">
-                                <div class="vstack gap-3">
-                                    <!-- Progress bar -->
-                                    <div class="overflow-hidden">
-                                        <div class="d-flex justify-content-between">
-                                            <p class="mb-1 h6">0/3 Completed</p>
-                                            <h6 class="mb-1 text-end">0%</h6>
-                                        </div>
-                                        <div class="progress progress-sm bg-primary bg-opacity-10">
-                                            <div class="progress-bar bg-primary aos" role="progressbar" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <!-- Course lecture -->
-                                    <div>
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div class="position-relative d-flex align-items-center">
-                                                <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static">
-                                                    <i class="fas fa-play me-0"></i>
-                                                </a>
-                                                <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-sm-400px">Introduction</span>
-                                            </div>
-                                            <p class="mb-0 text-truncate">2m 10s</p>
-                                        </div>
-                                        <hr class="mb-0">
-                                    </div>
+<!--                    <div class="accordion-item mb-3">-->
+<!--                        <h6 class="accordion-header font-base" id="heading-2">-->
+<!--                            <a class="accordion-button fw-bold collapsed rounded d-block pe-4" href="#collapse-2" data-bs-toggle="collapse" data-bs-target="#collapse-2" aria-expanded="false" aria-controls="collapse-2">-->
+<!--                                <span class="mb-0">Customer Life cycle</span>-->
+<!--                                <span class="small d-block mt-1">(3 Lectures)</span>-->
+<!--                            </a>-->
+<!--                        </h6>-->
+<!--                        <div id="collapse-2" class="accordion-collapse collapse" aria-labelledby="heading-2" data-bs-parent="#accordionExample2">-->
+<!--                            &lt;!&ndash; Accordion body START &ndash;&gt;-->
+<!--                            <div class="accordion-body mt-3">-->
+<!--                                <div class="vstack gap-3">-->
+<!--                                    &lt;!&ndash; Progress bar &ndash;&gt;-->
+<!--                                    <div class="overflow-hidden">-->
+<!--                                        <div class="d-flex justify-content-between">-->
+<!--                                            <p class="mb-1 h6">0/3 Completed</p>-->
+<!--                                            <h6 class="mb-1 text-end">0%</h6>-->
+<!--                                        </div>-->
+<!--                                        <div class="progress progress-sm bg-primary bg-opacity-10">-->
+<!--                                            <div class="progress-bar bg-primary aos" role="progressbar" data-aos="slide-right" data-aos-delay="200" data-aos-duration="1000" data-aos-easing="ease-in-out" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">-->
+<!--                                            </div>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    &lt;!&ndash; Course lecture &ndash;&gt;-->
+<!--                                    <div>-->
+<!--                                        <div class="d-flex justify-content-between align-items-center">-->
+<!--                                            <div class="position-relative d-flex align-items-center">-->
+<!--                                                <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static">-->
+<!--                                                    <i class="fas fa-play me-0"></i>-->
+<!--                                                </a>-->
+<!--                                                <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-sm-400px">Introduction</span>-->
+<!--                                            </div>-->
+<!--                                            <p class="mb-0 text-truncate">2m 10s</p>-->
+<!--                                        </div>-->
+<!--                                        <hr class="mb-0">-->
+<!--                                    </div>-->
 
-                                    <!-- Course lecture -->
-                                    <div>
-                                        <div class="d-flex justify-content-between align-items-center">
-                                            <div class="position-relative d-flex align-items-center">
-                                                <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static">
-                                                    <i class="fas fa-play me-0"></i>
-                                                </a>
-                                                <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-150px w-sm-400px"> What is Digital Marketing What is Digital Marketing</span>
-                                            </div>
-                                            <p class="mb-0 text-truncate">15m 10s</p>
-                                        </div>
-                                        <hr class="mb-0">
-                                    </div>
+<!--                                    &lt;!&ndash; Course lecture &ndash;&gt;-->
+<!--                                    <div>-->
+<!--                                        <div class="d-flex justify-content-between align-items-center">-->
+<!--                                            <div class="position-relative d-flex align-items-center">-->
+<!--                                                <a href="#" class="btn btn-danger-soft btn-round btn-sm mb-0 stretched-link position-static">-->
+<!--                                                    <i class="fas fa-play me-0"></i>-->
+<!--                                                </a>-->
+<!--                                                <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-150px w-sm-400px"> What is Digital Marketing What is Digital Marketing</span>-->
+<!--                                            </div>-->
+<!--                                            <p class="mb-0 text-truncate">15m 10s</p>-->
+<!--                                        </div>-->
+<!--                                        <hr class="mb-0">-->
+<!--                                    </div>-->
 
-                                    <!-- Course lecture -->
-                                    <div class="d-flex justify-content-between align-items-center">
-                                        <div class="position-relative d-flex align-items-center">
-                                            <a href="#" class="btn btn-light btn-round btn-sm mb-0 stretched-link position-static" data-bs-toggle="modal" data-bs-target="#coursePremium">
-                                                <i class="bi bi-lock-fill"></i>
-                                            </a>
-                                            <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-150px w-sm-400px">Type of Digital Marketing</span>
-                                        </div>
-                                        <p class="mb-0 text-truncate">18m 10s</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Accordion body END -->
-                        </div>
-                    </div>
+<!--                                    &lt;!&ndash; Course lecture &ndash;&gt;-->
+<!--                                    <div class="d-flex justify-content-between align-items-center">-->
+<!--                                        <div class="position-relative d-flex align-items-center">-->
+<!--                                            <a href="#" class="btn btn-light btn-round btn-sm mb-0 stretched-link position-static" data-bs-toggle="modal" data-bs-target="#coursePremium">-->
+<!--                                                <i class="bi bi-lock-fill"></i>-->
+<!--                                            </a>-->
+<!--                                            <span class="d-inline-block text-truncate ms-2 mb-0 h6 fw-light w-150px w-sm-400px">Type of Digital Marketing</span>-->
+<!--                                        </div>-->
+<!--                                        <p class="mb-0 text-truncate">18m 10s</p>-->
+<!--                                    </div>-->
+<!--                                </div>-->
+<!--                            </div>-->
+<!--                            &lt;!&ndash; Accordion body END &ndash;&gt;-->
+<!--                        </div>-->
+<!--                    </div>-->
 
                 </div>
                 <!-- Accordion END -->
@@ -526,12 +526,18 @@ export default {
     data(){
         return {
             courses: null,
+            accordion_states: []
         }
     },
     methods:{
         getMyCourses(){
             axios.post('api/courses/getCourse').then(res =>{
                 this.courses = res.data.courses
+                for(let c = 0; c < this.courses.length; c++){
+                    for(let l = 0; l < this.courses[c].lessons; l++){
+                        this.accordion_states.push()
+                    }
+                }
             });
         },
         getDays(days){
