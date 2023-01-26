9aa2f0ea40552cbae0e206cfbab32c4e8df6aa2e
Idris <astralgg@mail.ru>
2023-01-26 11:48:16 +0300

    add one lib and test changes on scratch compile

--- resources/js/views/compiler/Compiler.vue
+++ resources/js/views/compiler/Compiler.vue
@@ -1,10 +1,8 @@
 <template>
-    <iframe src="https://vscode.dev" width="100%" height="356" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+    <iframe allowtransparency="true" width="485" height="402" src="//scratch.mit.edu/projects/PROJECT-ID-HERE/embed?autostart=false" frameborder="0" allowfullscreen> </iframe>
 </template>
 <script>
-export default {
-    name: "Compiler"
-}
+
 </script>
 
 <style scoped>
