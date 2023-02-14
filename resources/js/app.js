ef281cf2244b10011ecfdb62ac94612306607f68
Idris <astralgg@mail.ru>
2023-02-14 16:59:17 +0300

    Big progress in work under immadiant solution teachers dream

--- resources/js/app.js
+++ resources/js/app.js
@@ -42,7 +42,6 @@ import moment from 'moment'
 import VueGoodTablePlugin from 'vue-good-table';
 import Vuelidate from 'vuelidate'
 
-
 Vue.use(Vuelidate)
 Vue.use(VueMoment)
 Vue.use(VueGoodTablePlugin)
